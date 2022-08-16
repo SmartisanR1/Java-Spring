b5f6fdd6f26d7742ef61ef0cf835b7211e17a495
hxq <huxueqiang@unicrypto.com.cn>
2022-08-16 10:46:25 +0800

    Jdbc

---
+++ chapter05/src/com/itheima/jdbc/TransactionTest.java
@@ -0,0 +1,29 @@
+package com.itheima.jdbc;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TransactionTest {
+    @Test
+    public void xmlTest() {
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        AccountDao accountDao =
+                (AccountDao) applicationContext.getBean("accountDao");
+        accountDao.transfer("Jack", "Rose", 100.0);
+        System.out.println("转账成功!");
+    }
+
+    @Test
+    public void annotationTest() {
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("applicationContext-annotation.xml");
+        //获取AccountDao实例
+        AccountDao accountDao =
+                (AccountDao) applicationContext.getBean("accountDao");
+        //调用实例中的转账方法
+        accountDao.transfer("Jack", "Rose", 100.0);
+        System.out.println("转账成功");
+    }
+}
